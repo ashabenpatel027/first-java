2a29cded38a46b04a6b1546bf12cf3fca9b9c632
ashabenpatel027 <ashabenpatel027@gmail.com>
2023-03-12 18:14:30 +0000

    commit

---
+++ src/MyDetails.java
@@ -0,0 +1,14 @@
+public class MyDetails {
+
+    static int age = 40;
+
+    static String name ="Asha";
+    public static void main(String[] args){
+        System.out.println(name);
+        System.out.println(age);
+
+
+
+
+
+
